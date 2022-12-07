df9acfbd6d542690c9974b9efda70b38c8c53f26
Stephen <wy1309374767@gmail.com>
2022-12-07 23:25:52 +0800

    proxy

--- src/main/java/tech/ityoung/spring/a11/ProxyDemo.java
+++ src/main/java/tech/ityoung/spring/a11/ProxyDemo.java
@@ -1,6 +1,9 @@
 package tech.ityoung.spring.a11;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
@@ -9,6 +12,18 @@ import java.lang.reflect.Proxy;
 @Slf4j
 public class ProxyDemo {
     public static void main(String[] args) {
+//        proxyTest();
+        Dog dog = new Dog();
+        Dog cgLibProxy = (Dog) Enhancer.create(Dog.class, (MethodInterceptor) (o, method, args1, methodProxy) -> {
+            log.info("cgLibProxy was born");
+            Object invoke = method.invoke(dog, args1);
+            log.info("cgLibProxy was dead");
+            return invoke;
+        });
+        cgLibProxy.live();
+    }
+
+    private static void proxyTest() {
         Dog dog = new Dog();
         Animal proxyInstance = (Animal) Proxy.newProxyInstance(ProxyDemo.class.getClassLoader(), new Class[]{Animal.class}, (proxy, method, args1) -> {
             log.info("animal was born");
